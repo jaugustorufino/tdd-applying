5a5e532a471811e823ca9dda767b45303c69fb61
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2023-08-19 21:12:18 -0300

    test: task creation

--- task-manager/src/test/java/TaskTest.java
+++ task-manager/src/test/java/TaskTest.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class TaskTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TaskTest {
+
+    @Test
+    public void testTaskCreation() {
+        Task task = new Task();
+        Assertions.assertNotNull(task);
+    }
 }
