8d5e5c8d7d980c83ea9d3c7386ef8928bcb02ff1
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2023-08-20 11:48:24 -0300

    feat: create task array list

--- task-manager/src/main/java/org/taskmanager/TaskList.java
+++ task-manager/src/main/java/org/taskmanager/TaskList.java
@@ -1,10 +1,21 @@
 package org.taskmanager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskList {
 
-    public TaskList() {}
+    private List<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<Task>();
+    }
+
+    public void addTask(Task newTask) {
+        this.tasks.add(newTask);
+    }
 
     public int size() {
-        return 0;
+        return tasks.size();
     }
 }
