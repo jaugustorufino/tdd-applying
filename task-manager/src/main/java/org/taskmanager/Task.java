9ae4ec94cefa14962d1f3fdd644ff29864c5d8ec
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2023-08-19 21:42:40 -0300

    feat: adds task attributes with getters

--- task-manager/src/main/java/org/taskmanager/Task.java
+++ task-manager/src/main/java/org/taskmanager/Task.java
@@ -1,5 +1,32 @@
 package org.taskmanager;
 
 public class Task {
-    public Task() {}
+
+    private String title;
+    private String description;
+    private String dueDate;
+    private String priority;
+
+    public Task(String title, String description, String dueDate, String priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
 }
