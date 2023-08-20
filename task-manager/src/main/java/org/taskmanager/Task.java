c7cfe0d9db243789f7ace4580d37c3d1be8bca77
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2023-08-19 21:44:50 -0300

    feat: adds task setters to update attributes

--- task-manager/src/main/java/org/taskmanager/Task.java
+++ task-manager/src/main/java/org/taskmanager/Task.java
@@ -18,15 +18,31 @@ public class Task {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getDueDate() {
         return dueDate;
     }
 
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
     public String getPriority() {
         return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
     }
 }
