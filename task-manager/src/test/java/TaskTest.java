ce9f6eacfd3c0addf8eab47cc3092a453fc77d41
Pedro Henrique <pedrohenriqueql.dev@gmail.com>
2023-08-19 21:43:30 -0300

    test: update task attributes

--- task-manager/src/test/java/TaskTest.java
+++ task-manager/src/test/java/TaskTest.java
@@ -15,4 +15,19 @@ public class TaskTest {
         Assertions.assertEquals(task.getDueDate(), "19/02/2023");
         Assertions.assertEquals(task.getPriority(), "Priority");
     }
+
+    @Test
+    public void testTaskUpdate() {
+        Task task = new Task("Title", "Description", "19/02/2023", "Priority");
+
+        task.setTitle("New Title");
+        task.setDescription("New Description");
+        task.setDueDate("29/05/2023");
+        task.setPriority("New Priority");
+
+        Assertions.assertEquals(task.getTitle(), "New Title");
+        Assertions.assertEquals(task.getDescription(), "New Description");
+        Assertions.assertEquals(task.getDueDate(), "29/05/2023");
+        Assertions.assertEquals(task.getPriority(), "New Priority");
+    }
 }
