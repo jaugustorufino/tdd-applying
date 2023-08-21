a42408fd53365670b1d4192e2b00047bcd30133f
Augusto Rufino <joseaorufino@gmail.com>
2023-08-21 19:27:27 -0300

    feat: add pagamentos field in the Invoice class

--- payment-processor/src/main/java/Invoice.java
+++ payment-processor/src/main/java/Invoice.java
@@ -1,14 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Invoice {
     private String nomeCliente;
     private double valor;
     private String data;
     private InvoiceStatus status;
+    private List<Payment> pagamentos;
 
     public Invoice(String nome, double valor, String data) {
         this.nomeCliente = nome;
         this.valor = valor;
         this.data = data;
         this.status = InvoiceStatus.NAO_PAGA;
+        this.pagamentos = new ArrayList<>();
     }
 
     public String getNomeCliente() {
@@ -29,5 +34,9 @@ public class Invoice {
 
     public void setStatus(InvoiceStatus status) {
         this.status = status;
+    }
+
+    public List<Payment> getPagamentos() {
+        return this.pagamentos;
     }
 }
