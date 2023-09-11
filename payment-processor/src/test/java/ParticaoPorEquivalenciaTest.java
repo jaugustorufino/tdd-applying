6e60f9fb740fb3c5434c2a4031210a4172b24290
jaugustorufino <joseaorufino@gmail.com>
2023-09-11 19:59:12 -0300

    test: adiciona testes funcionais usando particao por equivalencia

---
+++ payment-processor/src/test/java/ParticaoPorEquivalenciaTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ParticaoPorEquivalenciaTest {
+
+    private Fatura fatura;
+    private List<Boleto> listaDeBoletos1;
+    private List<Boleto> listaDeBoletos2;
+    private List<Boleto> listaDeBoletos3;
+    private ProcessadorPagamentos processador;
+
+    @BeforeEach
+    public void init() {
+        fatura = new Fatura("José", 800.00, "12312023");
+        listaDeBoletos1 = new ArrayList<>(Arrays.asList(
+                new Boleto("91232341231031", "12152023", 500.00),
+                new Boleto("82342341231032", "12102023", 299.99)
+        ));
+        listaDeBoletos2 = new ArrayList<>(Arrays.asList(
+                new Boleto("91232341231014", "12122023", 400.00),
+                new Boleto("82342341231012", "12132023", 400.00)
+        ));
+        listaDeBoletos3 = new ArrayList<>(Arrays.asList(
+                new Boleto("91232341231014", "12052023", 500.00),
+                new Boleto("82342341231011", "12102023", 600.00),
+                new Boleto("82342341231025", "12202023", 100.00)
+        ));
+        processador = new ProcessadorPagamentos();
+    }
+
+    @Test
+    public void testProcessaFaturaNaoPaga() {
+        processador.processaBoletos(fatura, listaDeBoletos1);
+
+        assertEquals(fatura.getStatus(), StatusFatura.NAO_PAGA);
+    }
+
+    @Test
+    public void testProcessaFaturaPagaValorIgual() {
+        processador.processaBoletos(fatura, listaDeBoletos2);
+
+        assertEquals(fatura.getStatus(), StatusFatura.PAGA);
+    }
+
+    @Test
+    public void testProcessaFaturaPagaAcimaValor() {
+        processador.processaBoletos(fatura, listaDeBoletos3);
+
+        assertEquals(fatura.getStatus(), StatusFatura.PAGA);
+    }
+}
